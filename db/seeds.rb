f1f6db14322ff07b23435fa63c34df3d6338b330
MinesJA <minesja@gmail.com>
2018-02-03 12:45:20 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+post1 = Post.create(title: "My First Post", description: "Check it it's my post")
+post2 = Post.create(title: "My Second Post", description: "Check it it's my second post")
